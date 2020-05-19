77010af118cbcf8d011a85cfaf785be0ca31f451
Caste <gabrielcastellano263@gmail.com>
2020-05-19 18:23:11 -0300

    puntaje

---
+++ BusinessLogic/Controllers/PuntajeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BusinessLogic.DataModel.Repositories;
+using Common.DataTransferObjects;
+using Persistencia.Database;
+
+namespace BusinessLogic.Controllers
+{
+	class PuntajeController
+	{
+		private readonly IMapper _mapper;
+		
+		public PuntajeController(IMapper mapper)
+		{
+			_mapper = mapper;
+		}
+
+		public PuntajeDTO GetByUsuarioAndJuego(int UsuarioId, int JuegoId)
+		{
+			using (ModelosDBContainer context = new ModelosDBContainer())
+			{
+				PuntajeRepository repositorio = new PuntajeRepository(context);
+				var entity = repositorio.Get(UsuarioId,JuegoId);
+				return this._mapper.Map<PuntajeDTO>(entity);
+			}
+		}
+
+		public HashSet<PuntajeDTO> GetAll()
+		{
+			HashSet<PuntajeDTO> puntajes = new HashSet<PuntajeDTO>();
+
+			using (ModelosDBContainer context = new ModelosDBContainer())
+			{
+				PuntajeRepository repositorio = new PuntajeRepository(context);
+				var entities = repositorio.GetAll();
+
+				foreach (var entity in entities)
+				{
+					puntajes.Add(this._mapper.Map<PuntajeDTO>(entity));
+				}
+			}
+			return puntajes;
+		}
+
+		public void Create(PuntajeDTO puntaje)
+		{
+			try
+			{
+				using (ModelosDBContainer context = new ModelosDBContainer())
+				{
+					PuntajeRepository repositorio = new PuntajeRepository(context);
+					
+					if (repositorio.AnyByUsuarioAndJuego(puntaje.Usuario.Id, puntaje.Juego.Id))
+					{
+						Puntaje p = repositorio.Get(puntaje.Usuario.Id, puntaje.Juego.Id);
+						if(p.Puntos < puntaje.Puntos)
+							p.Puntos = puntaje.Puntos;
+					}
+					else
+					{
+						repositorio.Create(this._mapper.Map<Puntaje>(puntaje));
+					}
+					context.SaveChanges();
+				}
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
+	}
+}
+
+
---
+++ BusinessLogic/DataModel/Repositories/PuntajeRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Persistencia.Database;
+using Common.DataTransferObjects;
+
+namespace BusinessLogic.DataModel.Repositories
+{
+	class PuntajeRepository
+	{
+		private readonly ModelosDBContainer _context;
+
+		public PuntajeRepository(ModelosDBContainer context)
+		{
+			this._context = context;
+		}
+
+		public Puntaje Get(int UsuarioId, int JuegoId)
+		{
+			var PuntajeEntity = this._context.PuntajeSet.Where(a => a.Usuario.Id == UsuarioId && a.Juego.Id == JuegoId).FirstOrDefault();
+			return PuntajeEntity;
+		}
+
+		public HashSet<Puntaje> GetAll()
+		{
+			var entitySet = this._context.PuntajeSet.Select(s => s).ToHashSet();
+			return entitySet;
+		}
+
+		public bool Any(int Id)
+		{
+			var exists = this._context.PuntajeSet.Any(s => s.Id == Id);
+			return exists;
+		}
+
+		public bool AnyByUsuarioAndJuego(int UsuarioId, int JuegoId)
+		{
+			var exists = this._context.PuntajeSet.Any(s => s.Usuario.Id == UsuarioId && s.Juego.Id == JuegoId);
+			return exists;
+		}
+
+		public void Create(Puntaje puntaje)
+		{
+			this._context.PuntajeSet.Add(puntaje);
+		}
+
+	}
+}
+
