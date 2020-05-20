e9f95b01c09862a621123568d4ca11bfd3452183
Isaac <48778675+Rodyu10@users.noreply.github.com>
2020-05-20 16:39:01 -0300

    Jugando

---
+++ BusinessLogic/DataModel/Repositories/JugandoRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Persistencia.Database;
+
+
+namespace BusinessLogic.DataModel.Repositories
+{
+    class JugandoRepository
+    {
+        private readonly ModelosDBContainer _context;
+
+        public JugandoRepository(ModelosDBContainer context)
+        {
+            this._context = context;
+        }
+
+        public Jugando Get(int Id)
+        {
+            var JugandoEntity = this._context.JugandoSet.Where(a => a.Id == Id).FirstOrDefault();
+            return JugandoEntity;
+        }
+
+        public HashSet<Jugando> GetAll()
+        {
+            var entitySet = this._context.JugandoSet.Select(s => s).ToHashSet();
+            return entitySet;
+        }
+
+
+        public bool Any(int Id)
+        {
+            var exists = this._context.JugandoSet.Any(s => s.Id == Id);
+            return exists;
+        }
+
+        public void Create(Jugando Juego)
+        {
+            this._context.JugandoSet.Add(Juego);
+        }
+
+        public void Delete(int Id)
+        {
+            var entity = this.Get(Id);
+            this._context.JugandoSet.Remove(entity);
+        }
+    }
+}
